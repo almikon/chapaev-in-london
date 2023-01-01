1a048b0dad0fab6d17a4aee1a53ca007f213cbe0
oldst <konyahin_al@mail.ru>
2023-01-01 22:26:33 +0300

    logs moved to the assets

--- packages/client/src/services/ServiceWorker.ts
+++ packages/client/src/services/ServiceWorker.ts
@@ -1,10 +1,12 @@
+import { logsMessages } from "../assets/logsMessages";
+
 export const startServiceWorker = () => {
     if ("serviceWorker" in navigator) {
         window.addEventListener("load", () => {
             navigator.serviceWorker.register("./sw.js").then(registration => {
-                console.log("ServiceWorker registration successful with scope: ", registration.scope);
+                console.log(logsMessages.succes, registration.scope);
             }).catch((error: string) => {
-                console.log("ServiceWorker registration failed: ", error);
+                console.log(logsMessages.fail, error);
             });
         });
     }
