7b5ca608e9395de6fa7f02d3f3144dbf6058fa6a
Aleksey Vasilev <alexvasoff@yandex.ru>
2023-01-17 21:18:00 +0300

    [FEAT] Show avatar in scoreCard

--- packages/client/src/components/scoreCard/ScoreCard.tsx
+++ packages/client/src/components/scoreCard/ScoreCard.tsx
@@ -1,21 +1,23 @@
 import { FC } from 'react';
+import { prepareAvatarLink } from '../../utils/prepareAvatarLink';
+import { Avatar } from '../UI-elements/Avatar/Avatar';
 import styles from './ScoreCard.module.sass';
 
 export type ScoreCardProps = {
   number: string | number;
-  avatar?: string;
+  avatar?: string | null;
   name: string;
   score: string | number;
 };
 
 export const ScoreCard: FC<ScoreCardProps> = (props) => {
 	const { number, name, score } = props;
-
+	const avatar = prepareAvatarLink(props.avatar);
 	return (
 		<div className={styles.scoreCard}>
 			<div className={styles.number}>{number}</div>
 			<div className={styles.user}>
-				<div className={styles.user__avatar}/>
+				<Avatar src={avatar} size={'small'} />
 				<div className={styles.user__name}>{name}</div>
 			</div>
 			<div className={styles.score}>{score}</div>
