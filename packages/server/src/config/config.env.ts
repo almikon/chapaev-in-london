fd8d7a8302977563ffe014f384a5d25f5bfccb5c
volkovanton71 <you@example.com>
2023-01-21 17:09:10 +0300

    fix location

--- packages/server/src/config/config.env.ts
+++ packages/server/src/config/config.env.ts
@@ -1,14 +1,18 @@
+import { config } from 'dotenv';
 import type { Dialect } from 'sequelize/types/sequelize';
 import { ARGV_INDEX } from '../assets/constants';
 
-// команда запуска сервиса
+config({
+	path: '../../.env'
+});
+
 export const ARGUMENTS = process.argv.slice(ARGV_INDEX);
 
-export const SERVER_PORT = Number(process.env.SERVER_PORT) || 3001;
+export const SERVER_PORT = Number(process.env.SERVER_PORT);
 export const DB_DIALECT: Dialect = process.env.DB_DIALECT as Dialect || 'postgres';
 export const DB_NAME = process.env.DB_NAME;
 export const DB_USER = process.env.DB_USER;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
-export const DB_PORT: number = Number(process.env.DB_PORT) || 5432;
+export const DB_PORT = Number(process.env.DB_PORT);
 export const DB_HOST = process.env.DB_HOST;
 
